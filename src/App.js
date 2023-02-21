553585b95e6070c49d4affeb9557bf40bcd3866a
Ilya Krapivnoy <ilya.krapivnoy@scelex.com>
2023-02-21 21:38:22 +0100

    add prettier

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import Accordion from "./components/accordion";
 import Editable from "./components/editable";
 
-function App() {
-    <div className='App'>
-        <header className='App-header'>
-            <Accordion title='Product Info' content='This is my content' />
-            <Editable />
-        </header>
-    </div>;
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Accordion title="Product Info" content="This is my content" />
+      <Editable />
+    </header>
+  </div>
+);
 
 export default App;
